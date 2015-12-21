fdba90c2cf942dacafd51904e4ebe70b623e9e44
Richard Kallos <rkallos@gmail.com>
2015-12-20 21:41:49 -0500

    Switched to (divisors) function from math/number-theory. Refactored (count-presents).

--- day20.rkt
+++ day20.rkt
@@ -1,28 +1,16 @@
 #lang racket
 
-(define (list-small-factors n [m 1])
-  (cond [(> m (sqrt n)) '()]
-        [(= 0 (remainder n m))
-         (cons m (list-small-factors n (add1 m)))]
-        [else (list-small-factors n (add1 m))]))
+(require math/number-theory)
 
-(define (list-factors n)
-  (let ([sf (list-small-factors n)])
-    (set->list (list->set
-     (append sf (map (lambda (x) (/ n x))
-                     (reverse sf)))))))
-
-(define (count-presents house)
+(define (count-presents house [part2 #f])
   (apply + (map (lambda (x) (* 10 x))
-                (list-factors house))))
-
-(define (count-presents2 house)
-  (apply + (map (lambda (x) (* 11 x))
-                (filter (lambda (y) (> (* y 50) house))
-                        (list-factors house)))))
+                (if part2
+                    (filter (lambda (y) (> (* y 50) house))
+                            (divisors house))
+                    (divisors house)))))
 
 (define (day-20 input [part2 #f])
   (let loop ([house 1])
-    (if (> ((if part2 count-presents2 count-presents) house) input)
+    (if (> (count-presents house part2) input)
         house
         (loop (add1 house)))))
